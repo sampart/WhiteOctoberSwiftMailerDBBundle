67f1df98ab763c15d3464b5511bdb5ed20dca2dc
Rich Sage <rich.sage@gmail.com>
2014-12-09 08:58:51 +0000

    Make queue flushing work with deferred explicit change tracking policy As per 622f38a

--- Spool/DatabaseSpool.php
+++ Spool/DatabaseSpool.php
@@ -130,12 +130,14 @@ class DatabaseSpool extends \Swift_ConfigurableSpool
         $time = time();
         foreach ($emails as $email) {
             $email->setStatus(EmailInterface::STATUS_PROCESSING);
+            $this->doc->getManager()->persist($email);
             $this->doc->getManager()->flush();
 
             $message = unserialize($email->getMessage());
             $count += $transport->send($message, $failedRecipients);
             if ($this->keepSentMessages === true) {
                 $email->setStatus(EmailInterface::STATUS_COMPLETE);
+                $this->doc->getManager()->persist($email);
             } else {
                 $this->doc->getManager()->remove($email);
             }
