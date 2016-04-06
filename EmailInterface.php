bdc5393dbbdf768138e7bda47a3713d01a11e897
Dennis Fridrich <fridrich.dennis@gmail.com>
2016-04-06 15:49:15 +0200

    Add setters to interface

--- EmailInterface.php
+++ EmailInterface.php
@@ -2,6 +2,10 @@
 
 namespace WhiteOctober\SwiftMailerDBBundle;
 
+/**
+ * Interface EmailInterface
+ * @package WhiteOctober\SwiftMailerDBBundle
+ */
 interface EmailInterface
 {
     const STATUS_FAILED = -1;
@@ -9,9 +13,33 @@ interface EmailInterface
     const STATUS_PROCESSING = 2;
     const STATUS_COMPLETE = 3;
 
+    /**
+     * @return string
+     */
     public function getMessage();
 
+    /**
+     * @return string
+     */
     public function getStatus();
 
+    /**
+     * @return string
+     */
     public function getEnvironment();
+
+    /**
+     * @param $message string Serialized \Swift_Mime_Message
+     */
+    public function setMessage($message);
+
+    /**
+     * @param $status string
+     */
+    public function setStatus($status);
+
+    /**
+     * @param $environment string
+     */
+    public function setEnvironment($environment);
 }
